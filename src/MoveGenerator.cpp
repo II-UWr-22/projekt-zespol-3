ef089c50dff07d6f25d0d84c2651f37ece9b98d9
Zuzanna Kurnicka <zuzannakurnicka@gmail.com>
2022-12-28 15:13:52 +0100

    Dodano ruch pionków i króla w pliku MoveGenerator.cpp

--- src/MoveGenerator.cpp
+++ src/MoveGenerator.cpp
@@ -1,7 +1,54 @@
 #include "MoveGenerator.h"
 
 void MoveGenerator::generateKingMoves(pair<int, int> square) {
-    // Zuzia
+    int row = square.first, column = square.second;
+
+    int dr[8]={1,1,0,-1, 1,-1,-1, 0};
+    int dc[8]={1,0,1,-1,-1, 1, 0,-1};
+
+    for(int i=0; i<8; ++i)
+    {
+        int new_row = row+dr[i], new_column = column+dc[i];
+        if(inBounds({new_row, new_column}))
+        {
+            // Move (and capture if opposite color)
+            if(board[new_row][new_column] == Piece::None ||
+            Piece::color(board[new_row][new_column]) != friendlyColor)
+            {
+                Move move = Move(row, column, new_row, new_column);
+                moves.push_back(move);
+            }
+        }
+    }
+
+    int castling_row = (friendlyColor == Piece::White)? 7 : 0;
+
+    // Short castling
+    if(!board.hasMoved[castling_row][4] && !board.hasMoved[castling_row][7] && 
+    board[castling_row][5] == Piece::None && board[castling_row][6] == Piece::None &&
+    !isSquareThreatened({castling_row, 4}, board) &&
+    !isSquareThreatened({castling_row, 5}, board))
+    {
+        Move move = Move(castling_row,4,castling_row,6);
+        moves.push_back(move);
+        // Move Rook
+        Move s_move = Move(castling_row,7,castling_row, 5);
+        move.secondaryMove = &s_move;
+    }
+
+    // Long castling
+    if(!board.hasMoved[castling_row][4] && !board.hasMoved[castling_row][0] && 
+    board[castling_row][1] == Piece::None && board[castling_row][2] == Piece::None &&
+    board[castling_row][3] == Piece::None &&
+    !isSquareThreatened({castling_row, 3}, board) &&
+    !isSquareThreatened({castling_row, 4}, board))
+    {
+        Move move = Move(castling_row,4,castling_row,2);
+        moves.push_back(move);
+        // Move Rook
+        Move s_move = Move(castling_row, 0, castling_row, 3);
+        move.secondaryMove = &s_move;
+    }
 }
 
 
@@ -111,6 +158,36 @@ void MoveGenerator::generatePawnMoves(pair<int, int> square) {
         moves.push_back(move);
     }
 
+    // Move two steps forward
+    if(inBounds({row+2*change, column}) && board[row+2*change][column] == Piece::None &&
+       board[row+1*change][column] == Piece::None) {
+        Move move = Move(row, column, row + 2*change, column);
+        moves.push_back(move);
+    }
+
+    // En Passant 1 
+    if(board.lastMove.startingSquare==std::make_pair(row+2*change, column+1) &&
+       board.lastMove.targetSquare==std::make_pair(row, column+1) &&
+       Piece::pieceType(board[row][column+1])==Piece::Pawn && 
+       Piece::color(board[row][column+1])!=friendlyColor){
+        Move move = Move(row, column, row + change, column+1);
+        moves.push_back(move);
+        // Opponent's Pawn disappears
+        Move s_move = Move(row, column+1, row, column+1);
+        move.secondaryMove = &s_move;
+    }
+
+    // En Passant 2 
+    if(board.lastMove.startingSquare==std::make_pair(row+2*change, column-1) &&
+       board.lastMove.targetSquare==std::make_pair(row, column-1) &&
+       Piece::pieceType(board[row][column-1])==Piece::Pawn && 
+       Piece::color(board[row][column-1])!=friendlyColor){
+        Move move = Move(row, column, row + change, column-1);
+        moves.push_back(move);
+        // Opponent's Pawn disappears
+        Move s_move = Move(row, column-1, row, column-1);
+        move.secondaryMove = &s_move;
+    }
 }
 
 void MoveGenerator::generatePseudoLegalMoves() {
