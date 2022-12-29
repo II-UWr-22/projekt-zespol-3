6ef6ee226441a6370bebdf6e7be81b9ecb680e54
piotr <piotr@piotrs-MacBook-Pro.local>
2022-12-29 15:51:55 +0100

    Poprawienie funkcji isSquareThreatened i generateMoves

--- src/MoveGenerator.cpp
+++ src/MoveGenerator.cpp
@@ -268,7 +268,7 @@ vector<Move> MoveGenerator::generateMoves(const Board &p_board) {
         
         for(int row : ROW) {
             for(int column : COLUMN) {
-                if(copy_board[row][column] == Piece::King &&
+                if(Piece::pieceType(copy_board[row][column]) == Piece::King &&
                 Piece::color(copy_board[row][column]) == friendlyColor ) rowKing = row, columnKing = column;
                 if(rowKing != -1 && columnKing != -1) break;
             }
@@ -293,17 +293,17 @@ bool MoveGenerator::isSquareThreatened(pair<int, int> square, Board& board) {
 
     //checking for pawns
     int change = friendlyColor == Piece::White? -1 : 1;
-    if(inBounds({row+change, column+1}) && board[row+change][column+1] == Piece::Pawn
+    if(inBounds({row+change, column+1}) && Piece::pieceType(board[row+change][column+1]) == Piece::Pawn
     && Piece::color(board[row+change][column+1]) != friendlyColor ) answer = 1;
 
-    if(inBounds({row+change, column-1}) && board[row+change][column-1] == Piece::Pawn
+    if(inBounds({row+change, column-1}) && Piece::pieceType(board[row+change][column-1]) == Piece::Pawn
     && Piece::color(board[row+change][column-1]) != friendlyColor ) answer = 1;
 
     //checking for king
     vector<pair<int, int>> directions{{0,1},{1,0},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
     for(auto current : directions) {
         int currentRow = row+current.first, currentColumn = column+current.second; 
-        if(inBounds({currentRow, currentColumn}) && board[currentRow][currentColumn] == Piece::King 
+        if(inBounds({currentRow, currentColumn}) && Piece::pieceType(board[currentRow][currentColumn]) == Piece::King 
         && Piece::color(board[currentRow][currentColumn]) != friendlyColor) answer = 1;
     }
     //checking for knights
@@ -311,7 +311,7 @@ bool MoveGenerator::isSquareThreatened(pair<int, int> square, Board& board) {
     directions = {{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2},{2,-1}};
     for(auto current : directions) {
         int currentRow = row+current.first, currentColumn = column+current.second; 
-        if(inBounds({currentRow, currentColumn}) && board[currentRow][currentColumn] == Piece::Knight 
+        if(inBounds({currentRow, currentColumn}) && Piece::pieceType(board[currentRow][currentColumn]) == Piece::Knight 
         && Piece::color(board[currentRow][currentColumn]) != friendlyColor) answer = 1;
     }
     //checking for bishops/queen
@@ -322,7 +322,7 @@ bool MoveGenerator::isSquareThreatened(pair<int, int> square, Board& board) {
         while(inBounds({currentRow, currentColumn}) && board[currentRow][currentColumn] == Piece::None)
             currentRow += current.first, currentColumn += current.second;
         if(inBounds({currentRow, currentColumn}) && Piece::color(board[currentRow][currentColumn]) != friendlyColor &&
-        (board[currentRow][currentColumn] == Piece::Bishop || board[currentRow][currentColumn] == Piece::Queen)) answer = 1;
+        (Piece::pieceType(board[currentRow][currentColumn]) == Piece::Bishop || Piece::pieceType(board[currentRow][currentColumn]) == Piece::Queen)) answer = 1;
     }
     //checking for rooks/queen
     directions.clear();
@@ -332,7 +332,7 @@ bool MoveGenerator::isSquareThreatened(pair<int, int> square, Board& board) {
         while(inBounds({currentRow, currentColumn}) && board[currentRow][currentColumn] == Piece::None)
             currentRow += current.first, currentColumn += current.second;
         if(inBounds({currentRow, currentColumn}) && Piece::color(board[currentRow][currentColumn]) != friendlyColor &&
-        (board[currentRow][currentColumn] == Piece::Rook || board[currentRow][currentColumn] == Piece::Queen)) answer = 1;
+        (Piece::pieceType(board[currentRow][currentColumn]) == Piece::Rook || Piece::pieceType(board[currentRow][currentColumn]) == Piece::Queen)) answer = 1;
     }
 
     return answer;
